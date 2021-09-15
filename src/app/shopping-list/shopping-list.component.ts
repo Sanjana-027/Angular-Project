967c6454d3c7da5da2504146b00cb0652bbf3a6c
Sanjana Singh <79395290+Sanjana-027@users.noreply.github.com>
2021-09-15 12:39:31 +0530

    Update shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,7 +4,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
+  templateUrl: './shopping-list.component.html'
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
